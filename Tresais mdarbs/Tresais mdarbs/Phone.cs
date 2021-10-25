d546ea4d33323d57bc607d19a450ef1b7400cdd8
Grensss <grenartur@gmail.com>
2021-10-25 20:26:24 +0300

    Phone

---
+++ Tresais mdarbs/Tresais mdarbs/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tresais_mdarbs
+{
+    class Phone
+    {
+        public string ModelName { get; set; }
+        public int Ram { get; set; }
+        public int Ghz { get; set; }
+        public int width { get ;set;}
+        public int length { get; set; }
+        public int height { get; set; }
+        
+
+        public string Parameters()
+        {
+            string Param = "Your " + ModelName + " have " + Ram + " Ram and " + Ghz + " Ghz processor, it's size(width, length, and height) in cm is "+ width + "/" + length + "/" + height + ". Nothing more, have good day :)" ;
+            return Param;
+        }
+    }
+}
---
+++ Tresais mdarbs/Tresais mdarbs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tresais_mdarbs
+{
+    class Program
+    {
+        static void First(string[] args)
+        {
+            Phone phone = new Phone();
+            Console.WriteLine("What is your phone manufacturer?");
+            phone.ModelName = Console.ReadLine();
+            Console.WriteLine("How many your phone have Ram?");
+            phone.Ram = int.Parse(Console.ReadLine());
+            Console.WriteLine("Thats all what i need :)");
+            phone.Ghz = 3;
+            phone.width = 10;
+            phone.length = 15;
+            phone.height = 17;
+            string param = phone.Parameters();
+            Console.WriteLine(param);
+
+        }
+        static void Second(string[] args)
+        {
+
+
+
+        }
+    }
+}
