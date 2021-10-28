1db18517569502824c34bc88b59c760f36337813
Grensss <grenartur@gmail.com>
2021-10-28 22:26:24 +0300

    thats all

--- Tresais mdarbs/Tresais mdarbs/Program.cs
+++ Tresais mdarbs/Tresais mdarbs/Program.cs
@@ -6,57 +6,57 @@ namespace Tresais_mdarbs
     {
         static void Main(string[] args)
         {
-            //Phone phone = new Phone();
-            //Console.WriteLine("What is your phone manufacturer?");
-            //phone.ModelName = Console.ReadLine();
-            //Console.WriteLine("How many your phone have Ram?");
-            //phone.Ram = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Thats all what i need :)");
-            //phone.Ghz = 3;
-            //phone.width = 10;
-            //phone.length = 15;
-            //phone.height = 17;
-            //string param = phone.Parameters();
-            //Console.WriteLine(param);
+            Phone phone = new Phone();
+            Console.WriteLine("What is your phone manufacturer?");
+            phone.ModelName = Console.ReadLine();
+            Console.WriteLine("How many your phone have Ram?");
+            phone.Ram = int.Parse(Console.ReadLine());
+            Console.WriteLine("Thats all what i need :)");
+            phone.Ghz = 3;
+            phone.width = 10;
+            phone.length = 15;
+            phone.height = 17;
+            string param = phone.Parameters();
+            Console.WriteLine(param);
 
 
 
-            //Car car = new Car();
-            //Console.WriteLine("What is your car model?");
-            //car.Model = Console.ReadLine();
-            //car.Seats = 4;
-            //car.Speed = 20;
-            //car.Numbcar = "HK - 2748";
-            //string cartext = car.Carametrs();
-            //Console.WriteLine(cartext);
-            //string carmanage = car.Carfunctionality();
-            //Console.WriteLine(carmanage);
-            //car.Functoin = Console.ReadLine();
-            //if (car.Functoin == "beep")
-            //{
-            //    Console.WriteLine ("Beep Beep");
-            //    Console.Beep();
-            //}
-            //else if(car.Functoin == "faster")
-            //{
-            //    car.Speed = car.Speed + 10;
-            //    Console.WriteLine("Your car speed is " + car.Speed + " km / h.");
-            //}
-            //else if (car.Functoin == "slower")
-            //{
-            //    car.Speed = car.Speed -10;
-            //    Console.WriteLine("Your car speed is " + car.Speed + " km / h.");
-            //}
+            Car car = new Car();
+            Console.WriteLine("What is your car model?");
+            car.Model = Console.ReadLine();
+            car.Seats = 4;
+            car.Speed = 20;
+            car.Numbcar = "HK - 2748";
+            string cartext = car.Carametrs();
+            Console.WriteLine(cartext);
+            string carmanage = car.Carfunctionality();
+            Console.WriteLine(carmanage);
+            car.Functoin = Console.ReadLine();
+            if (car.Functoin == "beep")
+            {
+                Console.WriteLine("Beep Beep");
+                Console.Beep();
+            }
+            else if (car.Functoin == "faster")
+            {
+                car.Speed = car.Speed + 10;
+                Console.WriteLine("Your car speed is " + car.Speed + " km / h.");
+            }
+            else if (car.Functoin == "slower")
+            {
+                car.Speed = car.Speed - 10;
+                Console.WriteLine("Your car speed is " + car.Speed + " km / h.");
+            }
 
-            //Product product = new Product();
-            //Console.WriteLine("Greetings write product name to search it's parameters.");
-            //product.Productname = Console.ReadLine();
-            //product.ProdHeight = 40;
-            //product.ProdLength = 20;
-            //product.ProdWeight = 5;
-            //product.ProdWidth = 60;
-            //string Prod = product.Productometrs();
-            //Console.WriteLine(Prod);
+            Product product = new Product();
+            Console.WriteLine("Greetings write product name to search it's parameters.");
+            product.Productname = Console.ReadLine();
+            product.ProdHeight = 40;
+            product.ProdLength = 20;
+            product.ProdWeight = 5;
+            product.ProdWidth = 60;
+            string Prod = product.Productometrs();
+            Console.WriteLine(Prod);
 
             Person person = new Person();
             Console.WriteLine("Greetings, do you want to complete some survey about you? Yes or No.");
